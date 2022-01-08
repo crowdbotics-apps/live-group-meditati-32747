33830194f5cdc7ea923881feb047d8e46fab4e1e
Crowdbotics <team@crowdbotics.com>
2022-01-08 20:42:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "live_group_meditati_32747",
+  url: "https://live_group_meditati_32747.botics.co",
+  api: "https://live_group_meditati_32747.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
